b232ffabd20c86a1796c1bd7cb6fb9f6d0b3e7d2
Juani <juanignacio.corrionero@gmail.com>
2022-04-18 00:16:03 -0300

    Ej 18 y 20

--- tp3/ej18/ej18.cpp
+++ tp3/ej18/ej18.cpp
@@ -12,27 +12,77 @@ La cantidad de movimientos del día 10.*/
 
 #include <iostream>
 using namespace std;
-int main() {
-int nummov, dia
-char tipo;
-float importe
-for (i=1; i<=14;i++){
-    cout << "dia " << i << endl;
-    cout << "Ingrese numero de movimiento: ";
-    cin >> nummov;
-    cout << endl << "Ingrese tipo 'E' - Extraccion / 'D' - Deposito)";
-    cin >> tipo;
-    cout << endl << "Ingrese el importe: ";
-    cin >> importe;
-    cout << endl;
+int main()
+{
+    int i, dia,condep = 0, conex = 0, maxidia, maximov, condia = 0;
+    char tipo;
+    float importe, acudep = 0,acuex = 0, maxi;
+    for (i=1; i<=14; i++)
+
+    {
+        cout << "Ingrese el dia (del 10 al 16): ";
+        cin >> dia;
+        if (dia == 10)
+        {
+            condia++;
+        }
+        cout << "Numero de movimiento: " << i;
+        cout << endl << "Ingrese tipo 'E' - Extraccion / 'D' - Deposito) ";
+        cin >> tipo;
+        tipo = toupper(tipo);
+        cout << "tipo: " << tipo << endl;
+        if (tipo == 'E')
+        {
+            conex++;
+        }
+        else
+        {
+            condep++;
+        }
+        cout << endl << "Ingrese el importe: ";
+        cin >> importe;
+        cout << endl;
+        if (tipo == 'D')
+        {
+            acudep += importe;
+        }
+        else
+        {
+            acuex += importe;
+        }
+
+
+        if (tipo == 'D')
+        {
+            if ( i == 1)
+            {
+                maxi = importe;
+                maxidia = dia;
+                maximov = i;
+
+            }
+            else
+            {
+                if (importe > maxi)
+                {
+                    maxi = importe;
+                    maxidia = dia;
+                    maximov = i;
+                }
+
+            }
+
+        }
 
 
 
+    }
 
+    cout << "El saldo final de la cuenta es " << acudep - acuex << endl;
+    cout << "El porcentaje de extracciones es " << ((float)conex * 100)/14 << "%" << endl;
+    cout << "El porcentaje de depositos es " << ((float)condep * 100)/14 << "%" << endl;
+    cout << "El depósito de mayor importe fue en el dia " << maxidia << ", el movimiento nro " << maximov << " de importe $" << maxi << endl;
+    cout << "El dia 10 se registraron " << condia << " movimientos." << endl;
 
-}
-
-
-
-return 0;
+    return 0;
 }
